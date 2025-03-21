88100f383d369fd53a376453140d53c59bdfb50e
Gabriel L. Kunz <58198156+kunzgabriel@users.noreply.github.com>
2025-03-20 21:47:55 -0300

    Update alunos.js

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -1,7 +1,10 @@
 const controller = require('../controllers/alunos');
+const authMiddleware = require('../middlewares/auth');
 
 module.exports = (app) => {
   app.get('/alunos', controller.buscarAlunos)
-  app.get('/alunos/:id([0-9]+)', controller.buscarAlunoPorId)
+  app.get('/alunos/:id([0-9]+)', authMiddleware.validaSenha, controller.buscarAlunoPorId)
+  app.get('/alunos/filtro', controller.buscarAlunoPorNome)
   app.post('/alunos', controller.cadastrarAluno)
+  app.patch('/alunos/:id([0-9]+)', controller.atualizarAluno)
 };
